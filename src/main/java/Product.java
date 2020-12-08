5dd193bf9f954951d05bf49cfb03c0885da838a3
深蓝 <mail@ri-co.cn>
2020-12-08 12:44:54 +0800

    完善后端产品列表

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -4,11 +4,15 @@ public class Product implements Serializable {
     //全公有，可以直接访问，不需要乱七八糟的访问方法了（懒
     public String name;
     public double price;
+    public String detail;
+    public String img;
 
     //构造方法
     public Product(){}
-    public Product(String name,double price){
+    public Product(String name,double price,String detail,String img){
         this.name=name;
         this.price=price;
+        this.detail=detail;
+        this.img=img;
     }
 }
--- src/main/java/ProductsListener.java
+++ src/main/java/ProductsListener.java
@@ -10,7 +10,8 @@ public class ProductsListener implements ServletContextListener {
     //暂无数据库，把商品信息放到List中
     public void contextInit(ServletContextEvent sce){
         ArrayList<Product>products=new ArrayList<Product>();
-        products.add(new Product("OPPO reno3",2899));
+        products.add(new Product("OPPO reno3",2899,"OPPO Reno3 Pro 双模5G 视频双防抖 90HZ高感曲面屏 7.7mm轻薄机身 12GB+256GB 雾月白全网通游戏视频手机","https://img10.360buyimg.com/n1/s450x450_jfs/t1/96127/30/8596/71921/5e05d2d3Eea057f1e/e596ae07921c890d.jpg"));
+        products.add(new Product("阿米洛机械键盘",938,"【个性定制】快速私人定制机械键盘 办公键盘 VA108键有线","https://img13.360buyimg.com//n0/jfs/t1/100616/13/2599/110353/5dd25383E3ab5cab7/d8383b1d1d5df8c2.jpg"));
         context=sce.getServletContext();
         context.setAttribute("list",products);
     }
